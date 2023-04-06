418f12f1191951f52b4cd3488947a87b311d2642
ThomasKennings <kennings@umich.edu>
2023-04-06 17:55:12 -0400

    working for most cases!

--- 50_degree_wedge_double_sensors/50_degree_wedge_double_sensors.ino
+++ 50_degree_wedge_double_sensors/50_degree_wedge_double_sensors.ino
@@ -12,11 +12,13 @@ const int deriv_arr_length = 1;                 // can be used to do moving aver
 const int tof_1_timing_budget = 100;            // Valid timing budgets: 15, 20, 33, 50, 100, 200 and 500 (ms)
 const int tof_2_timing_budget = 100;            // Valid timing budgets: 15, 20, 33, 50, 100, 200 and 500 (ms)
 const int deriv_accumulation_threshold = 15;
-const double acc_threshold_pos = 30;            // how high deriv must be to begin accumulation
-const double acc_threshold_neg = 30;            // how high deriv must be to begin accumulation
+const double acc_threshold_pos = 20;            // how high deriv must be to begin accumulation
+const double acc_threshold_neg = 20;            // how high deriv must be to begin accumulation
 const double deriv_debounce_threshold = 15;     // if deriv is below this, debounce is cancelled
 const int debounce_time_millis = 5000;          // when occupancy changes, how long to block continued accumulation in the same direction
-const double pair_timing_threshold_millis = 250;      // how close occupancy events must be for the most recent one to get undone (considered a double-count)
+const double pair_timing_threshold_millis = 300;      // how close occupancy events must be for the most recent one to get undone (considered a double-count)
+const int pos_dist_threshold = 1800;                // distance beyond which distance measurements are ignored
+const int neg_dist_threshold = 1800;                // distance beyond which distance measurements are ignored
 
 // Variables
 Adafruit_VL53L1X tof_1 = Adafruit_VL53L1X(XSHUT_PIN_1, GPIO_PIN_1);
@@ -48,6 +50,8 @@ int last_occupant_millis_1;
 int last_occupant_millis_2;
 int last_occupant_millis_1_actual;
 int last_occupant_millis_2_actual;
+int previous_last_occupant_millis_1_actual;
+int previous_last_occupant_millis_2_actual;
 int last_occupancy_sign_1;
 int last_occupancy_sign_2;
 int num_cycles = 0;
@@ -128,13 +132,13 @@ void loop() {
 
   // Accumulator logic for ToF 1
   if ((millis() - last_occupant_millis_1) > debounce_time_millis) {
-    if (deriv_arr_avg_1 > deriv_accumulation_threshold) {
+    if (deriv_arr_avg_1 > deriv_accumulation_threshold && dist_1 < pos_dist_threshold) {
       ++accumulator_pos_1;
     }
     else {
       accumulator_pos_1 = 0;
     }
-    if (deriv_arr_avg_1 < -deriv_accumulation_threshold) {
+    if (deriv_arr_avg_1 < -deriv_accumulation_threshold && dist_1 < neg_dist_threshold) {
       ++accumulator_neg_1;
     }
     else {
@@ -144,19 +148,22 @@ void loop() {
 
   // Accumulator logic for ToF 2
   if ((millis() - last_occupant_millis_2) > debounce_time_millis) {
-    if (deriv_arr_avg_2 > deriv_accumulation_threshold) {
+    if (deriv_arr_avg_2 > deriv_accumulation_threshold && dist_2 < pos_dist_threshold) {
       ++accumulator_pos_2;
     }
     else {
       accumulator_pos_2 = 0;
     }
-    if (deriv_arr_avg_2 < -deriv_accumulation_threshold) {
+    if (deriv_arr_avg_2 < -deriv_accumulation_threshold && dist_2 < neg_dist_threshold) {
       ++accumulator_neg_2;
     }
     else {
       accumulator_neg_2 = 0;
     }
   }
+
+  previous_last_occupant_millis_1_actual = last_occupant_millis_1_actual;
+  previous_last_occupant_millis_2_actual = last_occupant_millis_2_actual;
 
   // Occupancy event logic for ToF 1
   if (accumulator_pos_1 > acc_threshold_pos) {
@@ -190,32 +197,40 @@ void loop() {
     last_occupancy_sign_2 = -1;
     accumulator_neg_2 = 0;
     last_occupant_millis_2 = millis();
+    
     last_occupant_millis_2_actual = millis();
   }
   if (abs(deriv_arr_avg_2) < deriv_debounce_threshold) {
     last_occupant_millis_2 = 0;
   }
 
-  // Check whether both sensors triggered occupancy in the same direction in rapid succession. If so, undo the action of the most recent
-  if ((abs(last_occupant_millis_1_actual - last_occupant_millis_2_actual) < pair_timing_threshold_millis) && (last_occupancy_sign_1 == last_occupancy_sign_2)) {
+  // Check whether both sensors triggered occupancy in the same direction in rapid succession.
+  // Also check whether occupancy event has triggered this loop.
+  // If so, undo the action of the most recent
+  if ((abs(last_occupant_millis_1_actual - last_occupant_millis_2_actual) < pair_timing_threshold_millis) && 
+  ((previous_last_occupant_millis_1_actual != last_occupant_millis_1_actual) ||
+  (previous_last_occupant_millis_2_actual != last_occupant_millis_2_actual)) && 
+  (last_occupancy_sign_1 == last_occupancy_sign_2)) {
     if (last_occupant_millis_1_actual > last_occupant_millis_2_actual) {  // tof 1 triggered last
       occupancy -= last_occupancy_sign_1;
+      Serial.println("Undoing ToF 1");
     }
     if (last_occupant_millis_2_actual > last_occupant_millis_1_actual) {  // tof 2 triggered last
       occupancy -= last_occupancy_sign_2;
+      Serial.println("Undoing ToF 2");
     }
   }
 
 
   // Displaying debug information
-  Serial.print(1000);  // +/- constant so that serial plotter window doesn't shrink too much
-  Serial.print('\t');
-  Serial.print(-1000);
-  Serial.print('\t');
+  //Serial.print(1000);  // +/- constant so that serial plotter window doesn't shrink too much
+  //Serial.print('\t');
+  //Serial.print(-1000);
+  //Serial.print('\t');
   Serial.print("ToF 1: ");
   Serial.print(dist_1);
-  Serial.print('\t');
-  Serial.print(deriv_arr_avg_1);
+  //Serial.print('\t');
+  //Serial.print(deriv_arr_avg_1);
   Serial.print('\t');
   Serial.print(accumulator_pos_1);
   Serial.print('\t');
@@ -224,8 +239,8 @@ void loop() {
   Serial.print("ToF 2: ");
   Serial.print(dist_2);
   Serial.print('\t');
-  Serial.print(deriv_arr_avg_2);
-  Serial.print('\t');
+  //Serial.print(deriv_arr_avg_2);
+  //Serial.print('\t');
   Serial.print(accumulator_pos_2);
   Serial.print('\t');
   Serial.print(accumulator_neg_2);
@@ -233,8 +248,17 @@ void loop() {
   Serial.print("Occupancy: ");
   Serial.print(occupancy);
   Serial.print('\t');
-  Serial.print("Dist Diff %: ");
-  Serial.print(dist_diff_percent);
+  Serial.print("last_occupant_millis_1: ");
+  Serial.print(last_occupant_millis_1_actual);
+  Serial.print('\t');
+  Serial.print("last_occupant_millis_2: ");
+  Serial.print(last_occupant_millis_2_actual);
+  Serial.print('\t');
+  Serial.print("last_occupany_sign_1: ");
+  Serial.print(last_occupancy_sign_1);
+  Serial.print('\t');
+  Serial.print("last_occupany_sign_2: ");
+  Serial.print(last_occupancy_sign_2);
   Serial.println(" ");
 
   ++num_cycles;
